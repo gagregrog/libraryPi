cf9e4b2fc3229d9848ee3f6bc6db01ceb0b988dd
Robert Reed <robert.mc.reed@gmail.com>
2018-08-16 02:24:32 -0700

    scanner encapsulated, added info printing and "Selected"

--- barcode_scanner.py
+++ barcode_scanner.py
@@ -1,58 +1,75 @@
 from imutils.video import VideoStream
 from pyzbar import pyzbar
+from os import system
 import platform
 import time
 import cv2
-from handle_csv import CsvHandler
 import isbnlib
-from database import Database
 
 red = (0, 0, 255)
-csv = CsvHandler()
-db = Database()
+black = (0, 0, 0)
+video_width = 800
 
-print("[INFO] Starting stream...")
-camera = {"usePiCamera": True} if platform.system() == 'Linux' else {"src": 0}
-vs = VideoStream(**camera).start()
-time.sleep(1.0)
 
-while True:
-    frame = vs.read()
-    height, width = frame.shape[:2]
-    new_height = 800 / width * height
-    frame = cv2.resize(frame, (800, int(new_height)), interpolation=cv2.INTER_CUBIC)
+def start_scanner(db, csv, user):
+    system('clear')
+    print("[INFO] Starting stream...\n")
+    camera = {"usePiCamera": True} if platform.system() == 'Linux' else {"src": 0}
+    vs = VideoStream(**camera).start()
+    time.sleep(1.0)
+    last_book_found = {}
 
-    barcodes = pyzbar.decode(frame)
+    while True:
+        frame = vs.read()
+        height, width = frame.shape[:2]
+        new_height = int(video_width / width * height)
+        frame = cv2.resize(frame, (video_width, new_height), interpolation=cv2.INTER_CUBIC)
 
-    for barcode in barcodes:
-        isbn = barcode.data.decode("utf-8")
+        barcodes = pyzbar.decode(frame)
 
-        if isbnlib.notisbn(isbn):
-            continue
+        for barcode in barcodes:
+            isbn = barcode.data.decode("utf-8")
 
-        barcode_type = barcode.type
-        qr_code = barcode_type == 'QRCODE'
+            if isbnlib.notisbn(isbn):
+                continue
 
-        (x, y, w, h) = barcode.rect
+            qr_code = barcode.type == 'QRCODE'
 
-        if int(w) < 100 or int(h) < 40:
-            continue
+            (x, y, w, h) = barcode.rect
+            if int(w) < 100 or int(h) < 40:
+                continue
 
-        cv2.rectangle(frame, (x, y), (x + w, y + h), red, 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), red, 2)
 
-        display_name = csv.add_book(isbn, qr_code, db.add_book)
+            found_book = csv.add_book(isbn, qr_code, db.add_book)
 
-        if display_name:
-            cv2.putText(frame, display_name, (x, y - 20),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, red, 2)
+            if found_book:
+                cv2.putText(frame, found_book['display_name'], (x, y - 20),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, red, 2)
+                if found_book['isbn'] != last_book_found.get('isbn'):
+                    last_book_found = found_book
 
-    cv2.imshow("Barcode Scanner", frame)
-    key = cv2.waitKey(1) & 0xFF
+                    print("""Title: {}
+Authors: {}
+Year: {}
+Publisher: {}
 
-    if key == ord("q"):
-        break
+""".format(found_book['title'], found_book['authors'], found_book['year'], found_book['publisher']))
 
-print("[INFO] Closing stream...")
-csv.write_new_to_csv()
-cv2.destroyAllWindows()
-vs.stop()
+        cv2.putText(frame, user[0], (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, black, 2)
+        title = last_book_found.get('title')
+        if title:
+            cv2.putText(frame, "Selected: {}".format(title), (10, new_height - 20),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, red, 2)
+
+        cv2.imshow("Barcode Scanner", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord("q"):
+            break
+
+    print("[INFO] Closing stream...")
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
+    vs.stop()
