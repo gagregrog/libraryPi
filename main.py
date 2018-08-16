92b4cf49585ba0e461e33d050aa5a04f61bb4e01
Robert Reed <robert.mc.reed@gmail.com>
2018-08-16 00:42:49 -0700

    moved csv and db initialization to main

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from database import Database
+from handle_csv import CsvHandler
 import command_line
-db = Database()
 
-command_line.start_labs()
+db = Database()
+csv = CsvHandler()
+
+# command_line.start_labs()
 while True:
     user = command_line.handle_auth(db.add_user, db.check_credentials)
-    
+    command_line.greet(user, csv.get_display_data)
+    command_line.display_instructions()
